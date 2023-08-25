501f9e01a471d91e20bde8c01fe4d3bef35231e7
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2023-08-25 15:06:38 -0400

    User Registration with Some Minor Frontend Changes

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .models import User
 from .forms import UserForm
+from django.contrib import messages
 
 # Create your views here.
 
 def registerUser(request):
     if request.method == 'POST':
-        print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
             password = form.cleaned_data['password']
@@ -15,7 +15,10 @@ def registerUser(request):
             user.set_password(password)
             user.role = User.CUSTOMER
             user.save()
+            messages.success(request,'Your account has been registered successfully!')
             return redirect('registerUser')
+        else:
+            print(form.errors)
     else:
         form = UserForm()
     context = {
