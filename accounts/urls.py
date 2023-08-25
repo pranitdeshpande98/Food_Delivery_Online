c4c006eeef88a02d6187aa47a9f65e86ae850790
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2023-08-25 17:23:05 -0400

    Preliminary Vendor Application Added

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('registerUser/',views.registerUser,name='registerUser')
+    path('registerUser/',views.registerUser,name='registerUser'),
+    path('registerVendor/',views.registerVendor,name='registerVendor')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -25,3 +25,7 @@ def registerUser(request):
         'form' : form,
     }
     return render(request, 'accounts/registerUser.html', context)
+
+
+def registerVendor(request):
+    return render(request,'accounts/registerVendor.html')
