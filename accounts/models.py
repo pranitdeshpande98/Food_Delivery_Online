275233d5e58edea8d90ddd9cf40feb7ba8136a6e
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2023-08-26 14:54:10 -0400

    Vendor Registation Stage1

--- accounts/models.py
+++ accounts/models.py
@@ -38,11 +38,11 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
-    RESTAURANT = 1
+    VENDOR = 1
     CUSTOMER = 2
     ROLE_CHOICE = (
-        (RESTAURANT,'restaurant'),
-        (CUSTOMER,'customer'),
+        (VENDOR,'Vendor'),
+        (CUSTOMER,'Customer'),
     )
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
