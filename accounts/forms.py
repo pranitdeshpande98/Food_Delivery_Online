51efecbbe952f3ecc4d9fff8bae0aa4f2555f860
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2023-09-05 11:58:18 -0400

    Address Field Setup and Settings Upgradation Button Changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import User, UserProfile
-
+from .validators import allow_only_images_validator
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -18,3 +18,17 @@ class UserForm(forms.ModelForm):
                 "Password do not match!"
             )
 
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Start typing...','required':'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class' : 'btn btn-info'}),
+                                        validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class' : 'btn btn-info'}), validators=[allow_only_images_validator])
+    
+    latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address',
+                  'country','state',
+                  'city','pin_code','latitude','longitude']
