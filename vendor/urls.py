616b2019783318bb19d7e586a87f4cb12d044536
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2023-09-06 10:40:36 -0400

    Preliminary Menu Builing Dev Completion

--- vendor/urls.py
+++ vendor/urls.py
@@ -6,5 +6,6 @@ from accounts import views as AccountViews
 urlpatterns = [
     path('profile/',views.vprofile,name='vprofile'),
     path('',AccountViews.vendorDashboard,name='vendor'),
+    path('menu-builder/',views.menu_builder,name = 'menu_builder'),
     
 ]
