a4b5936ff9162b1df9f341a850e6a745dcf115e1
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2023-09-16 23:59:04 -0400

    Customer Profile Changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -32,3 +32,14 @@ class UserProfileForm(forms.ModelForm):
         fields = ['profile_picture', 'cover_photo', 'address',
                   'country','state',
                   'city','pin_code','latitude','longitude']
+        
+        def __init__(self, *args, **kwargs):
+            super(UserProfileForm, self).__init__(*args,**kwargs)
+            for field in self.fields:
+                if field == 'latitude' or field == 'longitude':
+                    self.fields[field].widget.attrs['readonly'] == 'readonly'
+
+class UserInfoForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','phone_number']
--- foodonline/settings.py
+++ foodonline/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'vendor',
     'menu',
     'marketplace',
+    'customers',
     'django.contrib.gis',
 ]
 
